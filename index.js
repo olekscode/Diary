2c617f407af53eb0fe0d1bfbc2ee161030ae575a
un-olk <olk.zaytsev@gmail.com>
2015-11-13 10:51:04 +0200

    Created index.js (sort of initial commit)

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+  // Send HTTP header
+  // HTTP Status 200: OK
+  // Content Type: text/plain
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  
+  // Send response body as "Hello, world!"
+  response.end("Hello, world!");
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
