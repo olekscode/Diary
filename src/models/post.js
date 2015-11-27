f1102d19e7bc57b7ea890a1ca4dc9bb9d7318722
un-olk <olk.zaytsev@gmail.com>
2015-11-28 01:15:28 +0200

    Replaced tabs with spaces in src/models

--- src/models/post.js
+++ src/models/post.js
@@ -1,7 +1,7 @@
 function Post(id, date, text){
-	this.id = id;
-	this.date = date;
-	//default value for title
-	this.title = "***";
-	this.text = text;
+    this.id = id;
+    this.date = date;
+    //default value for title
+    this.title = "***";
+    this.text = text;
 }
