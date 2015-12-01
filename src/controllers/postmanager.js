8a5276b5805f6f542110401192030003acceb64f
Mykhailo Huzyuk <g.and.m@ukr.net>
2015-12-01 13:25:48 +0200

    Сhanged the whole structure

--- src/controllers/postmanager.js
+++ src/controllers/postmanager.js
@@ -6,77 +6,45 @@ var User = require('../models/user');
 
 module.exports = (function() {
 
-    var _validate_login = function(login) {
-        return login.match('[^a-zA-Z0-9_\-]') == null;
-    };
-
-    var _validate_id = function(id) {
-	return id.match('[^0-9_\-]') == null;
-    };
-
-    var _is_login_available = function(login) {
-	return dataManager.getUser(login) == null;
-    };
-
     return {
-        get: function(id,login) {
-            if (_validate_id(id)) {
-                if (_is_login_available(login)) {
-                    dataManager.getPost(id);
-                    return "SUCCESS";
-                }
-                else {
-                    return "ACCESS DENIED";
-                }
-            }
-            else {
-                return "INVALID ID";
-            }
+        get: function(id) {
+            return (dataManager.get(id));
+            //if (res = "SUCCESS"){
+            //    return res;
+            //}
+            //else{
+            //	return "GET POST FAILED";
+            //}
         },
 
         create: function(pst,login) {
-            if (_validate_login(pst.id)) {
-                if (_is_login_available(login)) {
-                    dataManager.createPost(pst.id, pst.date,pst.title, pst.text);
-                    return "SUCCESS";
-                }
-                else {
-                    return "IS AVAILABLE. CREATE POST FAILED"
-                }
-            }
-            else {
-                return "INVALID LOGIN. UPDATE POST FAILED";
-            }
+            return (dataManager.createPost(pst.id, pst.date ,pst.title, pst.text));
+            //if (res = "SUCCESS"){
+            //    return res;
+            //}            
+            //else {
+            //    return "CREATE POST FAILED";
+            //}
         },
 
         update: function(pst,login) {
-            if (_validate_login(pst.id)) {
-                if (_is_login_available(login)) {
-                    dataManager.updatePost(pst.id, pst.date,pst.title, pst.text);
-                    return "SUCCESS";
-                }
-                else {
-                    return "IS AVAILABLE. UPDATE POST FAILED"
-                }
-            }
-            else {
-                return "INVALID LOGIN. UPDATE POST FAILED";
-            }
+            return (dataManager.updatePost(pst.id, pst.date,pst.title, pst.text));
+            //if (res = "SUCCESS"){
+            //    return res;
+            //}
+            //else {
+            //    return "UPDATE POST FAILED";
+            //}
         },
 
         remove: function(id,login) {
-            if (_validate_login(id)) {
-                if (_is_login_available(login)) {
-                    dataManager.createPost(id);
-                    return "SUCCESS";
-                }
-                else {
-                    return "IS AVAILABLE. REMOVE POST FAILED"
-                }
-            }
-            else {
-                return "INVALID LOGIN. REMOVE POST FAILED";
-            }
+            return (dataManager.createPost(id));
+            //if (res = "SUCCESS"){
+            //    return res;
+            //}
+            //else {
+            //    return " REMOVE POST FAILED";
+            //}
         }
     };
 })();
