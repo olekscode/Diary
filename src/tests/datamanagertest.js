87171269147082106e973916214cb94306ac8ecf
un-olk <olk.zaytsev@gmail.com>
2015-12-12 01:49:14 +0200

    Tried testing the DataManager. Found a bug (or made a mistake while testing)

--- src/tests/datamanagertest.js
+++ src/tests/datamanagertest.js
@@ -3,6 +3,23 @@ var Post = require('../models/post.js');
 var DataManager = require('../controllers/datamanager.js');
 
 var dataManager = new DataManager();
-dataManager.getPostById(2, function(post) {
-    console.log(post.getTitle() + ' ' + post.getText());
+
+//dataManager.getPostById(2, function(post) {
+//    console.log(post.getTitle() + ' ' + post.getText());
+//});
+
+//dataManager.getAllUsers(function(users) {
+//    for (var i = 0; i < users.length; ++i) {
+//	  console.log(users[i].getLogin() + ' ' + users[i].getPassword());
+//    }
+//});
+
+dataManager.getAllPosts(function(posts) {
+    for (var i = 0; i < posts.length; ++i) {
+	console.log(posts[i].getId() + ' '
+		   + posts[i].getLogin() + ' '
+		   + posts[i].getDate() + ' '
+		   + posts[i].getTitle() + ' '
+		   + posts[i].getText());
+    }
 });
